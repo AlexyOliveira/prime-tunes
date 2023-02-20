8c1faf64acb4ef40d480f5ceb602277d0e2223c7
alexyoliveira <cupofteaoficial@gmail.com>
2023-02-20 10:39:31 -0300

    I created routes and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/Search" component={ Search } />
+        <Route exact path="/Album/:id" component={ Album } />
+        <Route exact path="/Favorites" component={ Favorites } />
+        <Route exact path="/Profile" component={ Profile } />
+        <Route exact path="/Profile/Edit" component={ ProfileEdit } />
+        <Route exact path="" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
