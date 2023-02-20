cfa34939c2b5c1952c34f7aabbfa54b6238e4d6f
alexyoliveira <cupofteaoficial@gmail.com>
2023-02-20 11:49:57 -0300

    I created the header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 function Header() {
@@ -21,8 +22,16 @@ function Header() {
       {loading ? (
         <h2>Carregando...</h2>
       ) : (
-        <h3 data-testid="header-user-name">{user}</h3>
-      )}
+        <>
+          <h3 data-testid="header-user-name">{user}</h3>
+          <div className="links">
+            <nav>
+              <Link data-testid="link-to-search" to="/search">Search</Link>
+              <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+              <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+            </nav>
+          </div>
+        </>)}
     </div>
   );
 }
