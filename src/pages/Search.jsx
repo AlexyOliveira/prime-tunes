6bc6b1b1701f7459ce70ac50917c16fe04ade8c4
alexyoliveira <cupofteaoficial@gmail.com>
2023-02-20 12:06:17 -0300

    I created the field for searches

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 
 function Search() {
+  const [searchInput, setsearchInput] = useState('');
+  const searchLengthMin = 2;
+
+  const handleChange = ({ target }) => {
+    setsearchInput(target.value);
+  };
+
   return (
     <div data-testid="page-search">
       <Header />
-      Search
+      <form>
+        <input onChange={ handleChange } data-testid="search-artist-input" type="text" />
+        <button
+          disabled={ searchInput.length < searchLengthMin }
+          data-testid="search-artist-button"
+          type="button"
+        >
+          Pesquisar
+        </button>
+      </form>
     </div>
   );
 }
