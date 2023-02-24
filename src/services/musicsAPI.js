5816b988dc0177c10206e3c958b211d4afa24552
alexyoliveira <cupofteaoficial@gmail.com>
2023-02-24 14:57:30 -0300

    ..

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -1,5 +1,10 @@
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
+  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin': 'https://prime-tunes-67si3cdtk-alexyoliveira.vercel.app',
+    },
+  });
   const requestJson = await request.json();
   return requestJson.results;
 };
